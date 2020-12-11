843992e12993c366a0954bd681b13d332a50e9a2
vananh98 <35158086+vananh98@users.noreply.github.com>
2020-12-11 15:29:00 +0700

    oke

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,6 @@ Route::get('testMerge');
 Route::get('mergeBranch');
 Route::get('mergeBranch');
 Route::resource('anhmv', 'testApi');
-<<<<<<< HEAD
-<<<<<<< Updated upstream
 Route::get('anhmsdssds');
-=======
 Route::get('1');
 Route::get('12s2s5sd465fgh');
->>>>>>> Stashed changes
-=======
-Route::get('12s2s5sd465fgh);
->>>>>>> f0d0f52c560301ac539c56eff7845163e79d9821
